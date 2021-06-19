392597f1e5befc6bb7955a622a201bbf1024d4cd
Affalximam <62225185+affalximam@users.noreply.github.com>
2021-06-19 20:39:47 +0700

    Update script.js

--- Banjarnegara Xploit/assens/js/script.js
+++ Banjarnegara Xploit/assens/js/script.js
@@ -8,25 +8,36 @@ $(function(){
  });
  
  
-//Get the button:
+
+(function (){
+ 
+//mencari button scroll to top
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// mencari navbar
+var myNav = document.getElementById('navbar');
+
+// When the user scrolls down 20px
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    myNav.style.background = "black";
   } else {
     mybutton.style.display = "none";
+    myNav.style.background = "transparent";
   }
-}
+};
+
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+})();
 
 // preload
 var overlay = document.getElementById("overlay"); /* untuk mengambil elemen berdasarkan id yang ada id html */

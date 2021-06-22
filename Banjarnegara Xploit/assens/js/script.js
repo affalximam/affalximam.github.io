127f58b832cce4d965f85eb8bc0ad665925f6a6b
Affalximam <62225185+affalximam@users.noreply.github.com>
2021-06-22 10:31:55 +0700

    Update script.js

--- Banjarnegara Xploit/assens/js/script.js
+++ Banjarnegara Xploit/assens/js/script.js
@@ -7,29 +7,27 @@ $(function(){
      });
  });
  
- 
-
-(function (){
- 
-//mencari button scroll to top
-mybutton = document.getElementById("myBtn");
-
-// mencari navbar
-var myNav = document.getElementById('navbar');
-
 // When the user scrolls down 20px
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-    myNav.style.background = "black";
-  } else {
-    mybutton.style.display = "none";
-    myNav.style.background = "transparent";
+  if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+    $('#myBtn').css({
+      'display' : 'block'
+    });
+    $('#navbar').css({
+      'background' : '#000'
+    });
+  } 
+  else {
+    $('#myBtn').css({
+      'display' : 'none'
+    });
+    $('#navbar').css({
+      'background' : 'transparent'
+    });
   }
 };
-
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -37,7 +35,6 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-})();
 
 // preload
 var overlay = document.getElementById("overlay"); /* untuk mengambil elemen berdasarkan id yang ada id html */

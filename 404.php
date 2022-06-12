dee11b69d8741dcb88bd0c36f5623238bfaaa35f
Affalximam <62225185+affalximam@users.noreply.github.com>
2022-06-12 19:52:14 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,5 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="refresh" content="1; URL=https://affalximam.github.io/" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>redirect</title>
+	<meta http-equiv="refresh" content="2; URL=https://affalximam.github.io/" />
 </head>
+<body>
+	
+</body>
 </html>

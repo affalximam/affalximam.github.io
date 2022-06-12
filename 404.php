14621029ebd25c13dc8d27cca4d42a606341f09c
Affalximam <62225185+affalximam@users.noreply.github.com>
2022-06-12 19:49:35 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,10 +1,5 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/');
-	exit;
-?>
+<html>
+<head>
+<meta http-equiv="refresh" content="1; URL=https://affalximam.github.io/" />
+</head>
+</html>

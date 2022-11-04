f87a1118ae60beff0594584352c029e9017d7dde
Affalximam <62225185+affalximam@users.noreply.github.com>
2022-11-04 13:28:44 +0700

    Add files via upload

---
+++ affalximam v4/index.php
@@ -0,0 +1,10 @@
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/home/index.php?id=1');
+	exit;
+?>
